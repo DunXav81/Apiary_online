397a0acd44b6ed6262a8ddc6c573d79de645de16
Dan_Co <dva-2000@yandex.ru>
2022-11-19 19:33:02 +0300

    Formed models, conducted migrations. Configured the ability to work with tables from localhost\admin.

--- Apiary_online/scales_apiary/admin.py
+++ Apiary_online/scales_apiary/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Bienenkonigin_1, Weight_2, Weather_3
+
+admin.site.register(Bienenkonigin_1)
+admin.site.register(Weight_2)
+admin.site.register(Weather_3)
--- Apiary_online/scales_apiary/models.py
+++ Apiary_online/scales_apiary/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Bienenkonigin_1(models.Model):
+    beehive_number = models.IntegerField(default=1)
+    zeichen_konigin = models.IntegerField(default=1)
+    eiablage_seit = models.DateField('egg laying start date')
+    konigin_rasse = models.CharField(max_length=20)
+    konigin_linie = models.CharField(max_length=20)
+    muttervolk = models.CharField(max_length=20)
+    drohnenvolker = models.CharField(max_length=20)
+
+
+class Weight_2(models.Model):
+    beehive_number = models.ForeignKey(Bienenkonigin_1, on_delete=models.DO_NOTHING)
+    date_time_weight_fixation = models.DateTimeField('date and time of weighing')
+    weight_beehive = models.IntegerField(default=1)
+
+
+class Weather_3(models.Model):
+    date_time_fixing_values = models.DateTimeField('date and time of fixing weather values')
+    air_temperature_sensor = models.IntegerField(default=0)
+    air_temperature_api = models.IntegerField(default=0)
+    air_humidity_sensor = models.IntegerField(default=0)
+    air_humidity_api = models.IntegerField(default=0)
+    atmospheric_pressure_api = models.IntegerField(default=0)
+    wind_power_api = models.IntegerField(default=0)
+    wind_direction_api = models.CharField(max_length=20)
+    weather_description_api = models.CharField(max_length=100)
+
+# date_time_weight_fixation = models.ForeignKey(Weight_2, on_delete=models.CASCADE)
+# ▲ Попробовать в случае активации связей с Weight_2.date_time_weight_fixation
