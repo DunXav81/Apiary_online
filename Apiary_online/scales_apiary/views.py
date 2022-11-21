ed53053ecb2f89c1fe99dbb1b818079573841ebe
Dan_Co <dva-2000@yandex.ru>
2022-11-21 13:48:53 +0300

    Выведена с помощью шаблона meteo_data_table.html в браузер таблица с показанием всего множества значений датчика температуры.

--- Apiary_online/scales_apiary/views.py
+++ Apiary_online/scales_apiary/views.py
@@ -18,12 +18,29 @@ def main_page(request):
     ))
     '''
     
-    ''' ▼ Использование шаблона "meteo_data_table" для вывода в браузер таблицы метеоданных '''
+    ''' ▼ Использование шаблона "meteo_data_table" для вывода в браузер таблицы метеоданных
+          с одним значением в столбце
     meteo_data_all = Weather_3.objects.all()
     
     context = {
         #'meteo_data_0': str(meteo_data_all[0])
         'meteo_data_1': str(meteo_data_all[1])
+    }
+    
+    return render(
+        request,
+        'scales_apiary/meteo_data_table.html',
+        context
+    )
+    '''
+    
+    ''' ▼ Использование шаблона "meteo_data_table" для вывода в браузер таблицы метеоданных
+          с множеством значений в столбце '''
+    meteo_data_all = Weather_3.objects.all()
+    
+    context = {
+        #'meteo_data_0': str(meteo_data_all[0])
+        'meteo_data_array': meteo_data_all
     }
     
     return render(
